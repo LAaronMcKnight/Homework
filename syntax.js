c65d38a601e4274874857734638f72932296d64d
LAaronMcKnight <110464137+LAaronMcKnight@users.noreply.github.com>
2022-08-10 08:11:02 -0400

    add syntax

---
+++ syntax.js
@@ -0,0 +1,52 @@
+let myName = "Aaron"
+
+console.log('hello world')
+
+let firstValue = 12
+let secondValue = 17
+let sumValue = firstValue + secondValue
+
+console.log(sumValue)
+let newValue = sumValue * 3
+let newerValue = newValue - firstValue
+
+console.log(newerValue)
+
+//Find The Average
+
+let valOne = 1
+let valTwo = 2
+let valThree = 4
+let valFour = 7
+let valFive = 11
+let valSix = 16
+let valSeven = 22
+let valEight = 29
+let valNine = 38
+let valTen = 48
+
+//first five average
+
+let firstAvg = (valOne + valTwo + valThree + valFour + valFive) / 5
+
+console.log(firstAvg)
+
+//final five average
+
+let finalAvg = (valSix + valSeven + valEight + valNine + valTen) / 5
+
+console.log(finalAvg)
+
+//find average of above two results
+
+let endgameAvg = (firstAvg + finalAvg) / 2
+
+console.log(endgameAvg)
+
+newishAvg = (4 + 10 +22 +-30 +55) / 5
+
+console.log(newishAvg)
+
+newisherAvg = (68 + 7 +-22 + 9 + 100) / 5
+
+console.log(newisherAvg)
